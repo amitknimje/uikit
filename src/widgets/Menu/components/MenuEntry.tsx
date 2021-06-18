ba3dad2505fc59acff7f24bee133b238db33a93d
amitknimje <amitknimje@gmail.com>
2021-06-18 22:58:03 +0530

    Added

---
+++ src/widgets/Menu/components/MenuEntry.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import styled, { keyframes, DefaultTheme } from "styled-components";
+import { MENU_ENTRY_HEIGHT } from "../config";
+
+export interface Props {
+  secondary?: boolean;
+  isActive?: boolean;
+  theme: DefaultTheme;
+}
+
+const rainbowAnimation = keyframes`
+  0%,
+  100% {
+    background-position: 0 0;
+  }
+  50% {
+    background-position: 100% 0;
+  }
+`;
+
+const LinkLabel = styled.div<{ isPushed: boolean }>`
+  color: ${({ isPushed, theme }) => (isPushed ? "#141259" : "transparent")};
+  transition: color 0.4s;
+  flex-grow: 1;
+`;
+
+const MenuEntry = styled.div<Props>`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  height: ${MENU_ENTRY_HEIGHT}px;
+  padding: ${({ secondary }) => (secondary ? "0 32px" : "0 16px")};
+  font-size: ${({ secondary }) => (secondary ? "18px" : "20px")};
+  box-shadow: ${({ isActive, theme }) => (isActive ? `inset 5px 0px 0px #141259` : "none")};
+  text-shadow: 0px 5px 5px rgba(0, 0, 0, 0.25);
+  font-family: Fredoka One;
+  color: #141259;
+  a {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+  }
+
+  svg {
+    fill: ${({ theme }) => theme.colors.textSubtle};
+  }
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.tertiary};
+  }
+
+  // Safari fix
+  flex-shrink: 0;
+
+  &.rainbow {
+    background-clip: text;
+    animation: ${rainbowAnimation} 3s ease-in-out infinite;
+    background: ${({ theme }) => theme.colors.gradients.bubblegum};
+    background-size: 400% 100%;
+  }
+`;
+MenuEntry.defaultProps = {
+  secondary: false,
+  isActive: false,
+  role: "button",
+};
+
+const LinkLabelMemo = React.memo(LinkLabel, (prev, next) => prev.isPushed === next.isPushed);
+
+export { MenuEntry, LinkLabelMemo as LinkLabel };
